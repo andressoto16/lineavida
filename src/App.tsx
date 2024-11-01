b70c39ff376a45fb5a006e0c87690e7605ce8538
Carlosvillalobosunp <carlos.villalobos@unp.gov.co>
2024-11-01 14:42:41 -0500

    habilitacion tabla

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 import React, { useState } from 'react';
-import { TabVentana, VentanaUsuario } from 'eco-unp/ui';
+import { TabVentana, VentanaUsuario,CustomeTable } from 'eco-unp/ui';
+
+const columnas = [
+  { key: 'serial', label: 'Serial' },
+  { key: 'cedulaAnonima', label: 'Cedula'},
+  { key: 'departamento', label: 'Departamento' },
+  { key: 'ciudad', label: 'Ciudad'},
+  { key: 'nivel_riesgo', label: 'Nivel de Riesgo', hasModal: true },
+  { key: 'telefono_contactoAnonimo', label: 'Celular', hasModal: true },
+];
+
+const datos = [
+  { serial: 1, cedulaAnonima:'231523345', departamento: 'Caqueta',ciudad:'Florencia', nivel_riesgo: '1',  telefono_contactoAnonimo:'xxxxxx2315' },
+  { serial: 2, cedulaAnonima:'233456678', departamento: 'Nariño',ciudad:'Tumaco', nivel_riesgo: '2', telefono_contactoAnonimo:'xxxxxx2315' },
+];
 
 function App() {
   return (
@@ -8,8 +22,11 @@ function App() {
 
     {/* Tab para revision casos analista */}
     <TabVentana eventKey="Consultas" title="Consultas">
-    {/* <ContactForm />   */}
-    <h3>ventana para admin</h3>
+    <CustomeTable
+      columns={columnas}
+      data={datos}
+      // renderModalContent={RenderModalContent}
+   />
     </TabVentana>
 
     {/* Tab para consulta linea vida */}
